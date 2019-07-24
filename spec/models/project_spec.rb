e99e4a24a32ee5b40b50292ffe1fe1b83f86960f
Washington Silva <w-osilva@hotmail.com>
2019-07-24 14:03:48 -0300

    update model test: User, Project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "validate assossiations" do
-    should belong_to(:manager)
+  describe "associations" do
+    it { should belong_to(:manager) }
+    it { should have_many(:stories) }
   end
 
-  context "attributes" do
+  describe "attributes" do
     let(:attributes) { build(:project).attributes.symbolize_keys }
     it { expect(attributes).to include(:id, :name, :manager_id) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:manager) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "validate assossiations" do
-    should have_many(:projects)
+  describe "associations" do
+    it { should have_many(:projects) }
+    it { should have_many(:stories_as_requester).with_foreign_key('requester_id').class_name('Story') }
+    it { should have_many(:stories_as_owner).with_foreign_key('owner_id').class_name('Story') }
   end
 
-  context "attributes" do
+  describe "attributes" do
     let(:attributes) { build(:user).attributes.symbolize_keys }
     it { expect(attributes).to include(:id, :name, :email, :role) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
   end
 end
