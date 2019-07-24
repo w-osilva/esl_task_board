c82b7df02bb87c9943bbb523045f63fd2e3ecfb5
Washington Silva <w-osilva@hotmail.com>
2019-07-24 13:55:55 -0300

    set paper_trail user on controllers scope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_paper_trail_whodunnit
 
   protected
 
