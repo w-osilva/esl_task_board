00b8d95e5d9d3e862a23b0e6b8c7535135395ecc
Washington Silva <w-osilva@hotmail.com>
2019-07-25 16:39:59 -0300

    update story model and views

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,14 +1,13 @@
 class Story < ApplicationRecord
   belongs_to :project
   belongs_to :requester, class_name: 'User'
-  belongs_to :owner, class_name: 'User'
+  belongs_to :owner, class_name: 'User', optional: true
   has_many :tasks
 
   validates :name, presence: true
   validates :points, inclusion: { in: [nil, 1, 2, 3, 5, 8, 13] }
   validates :project, presence: true
   validates :requester, presence: true
-  validates :owner
   with_options unless: :pending? do |it|
     it.validates :started_at, presence: true
   end
@@ -20,6 +19,10 @@ class Story < ApplicationRecord
     it.validate :validate_finished_at_date
     it.validate :validate_status_accepted
   end
+
+  before_validation :autoset_started_at, unless: :pending?
+  before_validation :autoset_finished_at, if: :accepted?
+  before_save :autounset_finished_at, unless: :accepted?
 
   extend Enumerize
   enumerize :status, in: [:pending, :started, :delivered, :accepted ], default: :pending, predicates: true, scope: true
@@ -47,4 +50,15 @@ class Story < ApplicationRecord
     end
   end
 
+  def autoset_started_at
+    self[:started_at] ||= Time.now
+  end
+
+  def autoset_finished_at
+    self[:finished_at] ||= Time.now
+  end
+
+  def autounset_finished_at
+    self[:finished_at] = nil
+  end
 end
