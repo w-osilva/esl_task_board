318ed1e2c22f39bccbb8877bc7046b0ed8aede1a
Washington Silva <w-osilva@hotmail.com>
2019-07-25 21:57:06 -0300

    custom input to datepicker

---
+++ app/inputs/tempus_dominus_input.rb
@@ -0,0 +1,32 @@
+class TempusDominusInput < SimpleForm::Inputs::Base
+  def input(wrapper_options)
+    template.content_tag(:div, class: 'input-group date', data: { target_input: 'nearest' }, id: "#{object_name}_#{attribute_name}") do
+      template.concat @builder.text_field(attribute_name, input_html_options)
+      template.concat div_button
+    end
+  end
+
+  def input_html_options
+    super.merge({class: 'form-control datetimepicker-input'})
+  end
+
+  def div_button
+    template.content_tag(:div, class: 'input-group-append', data: {target: "##{object_name}_#{attribute_name}", toggle: 'datetimepicker'} ) do
+      template.concat span_table
+    end
+  end
+
+  def span_table
+    template.content_tag(:div, class: 'input-group-text') do
+      template.concat icon_table
+    end
+  end
+
+  def icon_remove
+    "<i class='glyphicon glyphicon-remove'></i>".html_safe
+  end
+
+  def icon_table
+    "<i class='fa fa-calendar'></i>".html_safe
+  end
+end
